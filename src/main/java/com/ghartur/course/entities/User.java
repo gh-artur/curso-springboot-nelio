49e87c8f26dd3539c6f64737239d0be246f3de74
Artur Santos <arturfrancisco86@gmail.com>
2022-09-13 18:19:40 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/ghartur/course/entities/User.java
+++ src/main/java/com/ghartur/course/entities/User.java
@@ -10,11 +10,11 @@ import javax.persistence.Id;
 
 @Entity(name="users")
 public class User implements Serializable {
-
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -23,7 +23,7 @@ public class User implements Serializable {
 	public User() {
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -32,11 +32,11 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
