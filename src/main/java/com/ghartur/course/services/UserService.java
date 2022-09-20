47bcfd5f456f3d01821d73af6e678eec6dc3fccf
Artur Santos <arturfrancisco86@gmail.com>
2022-09-20 12:55:37 -0300

    Exception handling - findById

--- src/main/java/com/ghartur/course/services/UserService.java
+++ src/main/java/com/ghartur/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.ghartur.course.entities.User;
 import com.ghartur.course.repositories.UserRepository;
+import com.ghartur.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()->new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
@@ -43,5 +44,6 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
+	
 	
 }
